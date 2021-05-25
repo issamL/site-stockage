411637b2bca4d442759307378f83e67ea0b64077
issamL <83637069+issamL@users.noreply.github.com>
2021-05-25 11:41:09 +0200

    Update myTest.php

--- tests/myTest.php
+++ tests/myTest.php
@@ -2,10 +2,8 @@
 use PHPUnit\Framework\TestCase;
 
 class myTest extends Testcase{
-    /*
-     * testing some functions
-     */
-
+   
+    /** @test */
     public function inputSettingsEmpty(){
         include('functions.php');
         $result = isEmptyInfo('!null','!null','!null');
