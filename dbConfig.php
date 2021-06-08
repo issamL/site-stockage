be9a41c178d7ae7d070f053c17143fdb6865a76a
issamL <lhous.issam99@gmail.com>
2021-06-08 13:22:37 +0200

    adding port

--- dbConfig.php
+++ dbConfig.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new PDO("mysql:host=database-2.cpjphvxsypnw.eu-west-3.rds.amazonaws.com;dbname=userlogin", "admin", "kharabacha2021");
+$conn = new PDO("mysql:host=database-2.cpjphvxsypnw.eu-west-3.rds.amazonaws.com;dbname=userlogin;port=3306", "admin", "kharabacha2021");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
