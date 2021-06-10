177bd1bbb3cb60e29490b3b28834a1bf8abe3814
issamL <lhous.issam99@gmail.com>
2021-06-10 09:59:33 +0200

    configuration with circleci

--- dbConfig.php
+++ dbConfig.php
@@ -1,6 +1,11 @@
 <?php
-
-$conn = new PDO("mysql:host=database-2.cpjphvxsypnw.eu-west-3.rds.amazonaws.com;dbname=userlogin;", "admin", "kharabacha2021");
+try {
+    $conn = new PDO("mysql:host=database-2.cpjphvxsypnw.eu-west-3.rds.amazonaws.com;dbname=userlogin;",
+        "admin",
+        "kharabacha2021");
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
